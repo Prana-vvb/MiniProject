defa5d30a52bb575e19a5fcc667fadaf696f78d1
Prana-vvb <pvb1532005@gmail.com>
2023-12-04 11:15:03 +0530

    Modified size and speeds of Ally and Enemy classes

--- Project1.py
+++ Project1.py
@@ -73,13 +73,15 @@ class Player(Actors):
 class Enemies(Actors):
     def __init__(self, ashape, color, startX, startY):
         Actors.__init__(self, ashape, color, startX, startY)
-        self.speed = 3
+        self.shapesize(stretch_wid=0.75, stretch_len=0.75)
+        self.speed = 5
         self.setheading(random.randint(0, 360))
 
 #Friendlies. Child of Actors
 class Ally(Actors):
     def __init__(self, ashape, color, startX, startY):
         Actors.__init__(self, ashape, color, startX, startY)
+        self.shapesize(stretch_wid=0.75, stretch_len=0.75)
         self.speed = 4
         self.setheading(random.randint(0, 360))
 
